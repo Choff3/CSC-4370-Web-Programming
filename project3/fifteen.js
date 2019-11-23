0cd95bfa55d5bc30b2dbecfc1f77d3b2a4965488
Teddy Hoff <teddyhoff@gmail.com>
2019-11-23 15:55:36 -0500

    comments

--- project3/fifteen.js
+++ project3/fifteen.js
@@ -1,181 +1,125 @@
-//"use strict"; //this activates strict mode 
-
-//globally declared variables 
-var gamePiece; 
-var notify;
-var timer;
-var spaceY;
-var spaceX;
-
+var tiles; 
+var posY;
+var posX;
+//variables for storing tiles and positions
 
  window.onload = function (){
-	var puzzleArea = document.getElementById('puzzlearea');
-	gamePiece = puzzleArea.getElementsByTagName('div'); //retrieve element within puzzlearea
+	var board = document.getElementById('board');
+	tiles = board.getElementsByTagName('div');
+	//set varible for board and fill tiles array
 
-	for (var i=0; i<gamePiece.length; i++){ //applies features to each puzzle piece 
+	for (var i=0; i<tiles.length; i++){
 
-		gamePiece[i].className = 'puzzlepiece'; //setting up the puzzle piece code
-		gamePiece[i].style.left = (i%4*100)+'px'; //calculates the position for puzzle pieces from the left of the screen
-		gamePiece[i].style.top = (parseInt(i/4)*100) + 'px'; //calculates the position for puzzle pieces from the top of the screen
-		gamePiece[i].style.backgroundPosition= '-' + gamePiece[i].style.left + ' ' + '-' + gamePiece[i].style.top; 
-		//calculates the position of the background picture so in moves in relation to the puzzle pieces
+		tiles[i].className = 'tile';
+		tiles[i].style.left = (i%4*100)+'px'; 
+		tiles[i].style.top = (parseInt(i/4)*100) + 'px';
+		//calculates tile position from the left and top
+		tiles[i].style.backgroundPosition= '-' + tiles[i].style.left + ' ' + '-' + tiles[i].style.top; 
+		//makes the image move with the tile
 
-		gamePiece[i].onmouseover = function(){ //aplies features when mouse moves over puzzle pieces
-			if (checkMove(parseInt(this.innerHTML))){ //checks whenever a move is made
-				this.style.border = "3px solid red"; //changes to red when a puzzle piece is near an empty space
-				this.style.color = "#006600"; //text color changes to green when a puzzle piece is near an empty space
-				this.style.textDecoration = "underline"; //underlines the number of the puzzle piece piece
-                this.style.backgroundImage="url('background.jpg')"; 
-                //sets the image for the puzzle's background 
+		tiles[i].onmouseover = function(){
+			if (checkMove(parseInt(this.innerHTML))){
+				this.className = "moveableTile";
 			}
 		};
-
-
-		gamePiece[i].onmouseout = function(){ //activates whenever mouse moves out of puzzle piece
-			this.style.border = "2px solid black"; //reverts to its original size border 
-			this.style.color = "#000000"; //reverts to original text color
-			this.style.textDecoration = "none"; //reverts to original text state
+		tiles[i].onmouseout = function(){
+			this.className = "tile";
 		};
+		//changes style of tiles when moused over and out
 
-
-
-		gamePiece[i].onclick = function(){ //activates when mouse clicks on a puzzle piece
-
-			if (checkMove(parseInt(this.innerHTML))){ //checks whether or not the puzzle piece can move into an empty space
-
-				swap(this.innerHTML-1); //moves into an empty space if true
-				if (finish()){ //checks when the all the 15 pieces are in its right space
-					win(); //alerts the player that they have won the game
+		tiles[i].onclick = function(){
+			if (checkMove(parseInt(this.innerHTML))){ //checks if the tile can move
+				swap(this.innerHTML-1); //moves the tile into the empty space
+				if (finish()){ //checks if the board is finished
+					win();
 				}
 				return;
 			}
 		};
 	}
 
-	var shuffle = document.getElementById('shufflebutton'); //initializes the shuffle button
-	spaceX = '300px'; 
-	spaceY = '300px';
-	shuffle.onclick = function(){ //activates whenever the shuffle button is clicked
+	posX = '300px'; 
+	posY = '300px';
 
+	document.getElementById('shuffleBtn').onclick = function(){
 		for (var i=0; i<300; i++){
-
-			var rand = parseInt(Math.random()* 100) %4; //generates a random number for shuffling each piece
-
+			var rand = parseInt(Math.random()* 100) %4;
 			if (rand == 0){
-
-				var temp = up(spaceX, spaceY); 
-
+				var temp = up(posX, posY);
 				if ( temp != -1){
 					swap(temp);
 				}
 			}
 			if (rand == 1){
 
-				var temp = down(spaceX, spaceY);
+				var temp = down(posX, posY);
 
 				if ( temp != -1){
 					swap(temp);
 				}
 			}
 			if (rand == 2){
-				var temp = left(spaceX, spaceY);
+				var temp = left(posX, posY);
 				if ( temp != -1){
 					swap(temp);
 				}
 			}
 			if (rand == 3){
-				var temp = right(spaceX, spaceY);
+				var temp = right(posX, posY);
 				if (temp != -1){
 					swap(temp);
 				}
 			}
 		}
 	};
+	//When the shuffle button is clicked, a random number between 1 and 4 is generated
+	//This number randomly swaps tiles around by calling the swap function repeatedly in different directions
 };
 
-
-
-function checkMove(position){ // returns true whenever a piece can be moved into an empty space
-	if (left(spaceX, spaceY) == (position-1)){
+function checkMove(position){
+	if (left(posX, posY) == (position-1)){
 		return true;
 	}
-	if (down(spaceX, spaceY) == (position-1)){
+	if (down(posX, posY) == (position-1)){
 		return true;
 	}
-	if (up(spaceX, spaceY) == (position-1)){
+	if (up(posX, posY) == (position-1)){
 		return true;
 	}
-	if (right(spaceX, spaceY) == (position-1)){
+	if (right(posX, posY) == (position-1)){
 		return true;
 	}
 }
+//returns true if a tile is next to the empty space
 
-
-function Notify(){ //notifies the user 
-	notify --; //decrements the value of 
-
-	if (notify == 0){ //if the value reaches the end then
-
-		var body = document.getElementsByTagName('body'); //retrieves body element in html
-		body[0].style.backgroundImage= "none"; //reverts to original page background
-		alert('Winner! ... Shuffle and Play Again'); //tells the user that they have won the game 
-		var para=document.getElementsByClassName('explanation');
-	    para[0].style.visibility="visible"; //reverts visiblity to its original state
-		return;
-	}
-	else  (notify % 2) 
-	{
-
-		var body = document.getElementsByTagName('body'); 
-	    body[0].style.backgroundImage= "url('win.jpg')";
-	    //sets background pic to show user that they had completed the puzzle
-	}
-    timer= setTimeout(Notify, 200); //notifies the user for 2 secs
+function win(){
+	document.body.style.background = "url('win.jpg')";
+	document.getElementById('rules').className = "winText";
+	document.getElementById('rules').innerHTML = "You Win!";
 }
+//change the page if the user wins
 
-
-
-function win(){ //notifies user that they have won
-
-	var body = document.getElementsByTagName('body');
-	body[0].style.backgroundImage= "url('win.jpg')";
-	notify = 10; //initializes notify variable
-	timer= setTimeout(Notify, 200);
-	var para=document.getElementsByClassName('explanation');
-	para[0].style.visibility="hidden"; //hides text when user is being notified
-
-}
-
-
-function finish(){ //checks when the game reaches its end
-
+function finish(){
 	var flag = true;
-	for (var i = 0; i < gamePiece.length; i++){ //for each puzzle piece 
-
-		var top = parseInt(gamePiece[i].style.top);
-		var left = parseInt(gamePiece[i].style.left);
-
-		if (left != (i%4*100) || top != parseInt(i/4)*100){ //checks if each piece matches its left and top position
-
+	for (var i = 0; i < tiles.length; i++){
+		var top = parseInt(tiles[i].style.top);
+		var left = parseInt(tiles[i].style.left);
+		if (left != (i%4*100) || top != parseInt(i/4)*100){
 			flag = false;
 			break;
 		}
 	}
 	return flag;
 }
+//if all tiles match their proper position, the game is finished and this function returns true
 
-
-
-function left(x, y){ //calculates how far to the left a puzzlepiece should position
-
+function left(x, y){
 	var cordX = parseInt(x);
 	var cordY = parseInt(y);
-
 	if (cordX > 0){
+		for (var i = 0; i < tiles.length; i++){
 
-		for (var i = 0; i < gamePiece.length; i++){
-
-			if (parseInt(gamePiece[i].style.left) + 100 == cordX && parseInt(gamePiece[i].style.top) == cordY){
+			if (parseInt(tiles[i].style.left) + 100 == cordX && parseInt(tiles[i].style.top) == cordY){
 				return i;
 			} 
 		}
@@ -184,18 +128,14 @@ function left(x, y){ //calculates how far to the left a puzzlepiece should posit
 		return -1;
 	}
 }
+//determines how far the tile position should change to the left
 
-
-
-function right (x, y){ //calculates how far to the right a puzzlepiece should position
-
+function right (x, y){
 	var cordX = parseInt(x);
 	var cordY = parseInt(y);
 	if (cordX < 300){
-
-		for (var i =0; i<gamePiece.length; i++){
-
-			if (parseInt(gamePiece[i].style.left) - 100 == cordX && parseInt(gamePiece[i].style.top) == cordY) {
+		for (var i =0; i<tiles.length; i++){
+			if (parseInt(tiles[i].style.left) - 100 == cordX && parseInt(tiles[i].style.top) == cordY) {
 				return i;
 			}
 		}
@@ -204,19 +144,14 @@ function right (x, y){ //calculates how far to the right a puzzlepiece should po
 		return -1;
 	} 
 }
+//determines how far the tile position should change to the right
 
-
-
-function up(x, y){ //calculates how far up a puzzlepiece should position
-
+function up(x, y){
 	var cordX = parseInt(x);
 	var cordY = parseInt(y);
-
 	if (cordY > 0){
-
-		for (var i=0; i<gamePiece.length; i++){
-
-			if (parseInt(gamePiece[i].style.top) + 100 == cordY && parseInt(gamePiece[i].style.left) == cordX) {
+		for (var i=0; i<tiles.length; i++){
+			if (parseInt(tiles[i].style.top) + 100 == cordY && parseInt(tiles[i].style.left) == cordX) {
 				return i;
 			}
 		} 
@@ -225,18 +160,15 @@ function up(x, y){ //calculates how far up a puzzlepiece should position
 		return -1;
 	}
 }
+//determines how far up the tile position should change
 
-
-
-function down (x, y){ //calculates how far down a puzzlepiece should position
-
+function down (x, y){
 	var cordX = parseInt(x);
 	var cordY = parseInt(y);
 	if (cordY < 300){
+		for (var i=0; i<tiles.length; i++){
 
-		for (var i=0; i<gamePiece.length; i++){
-
-			if (parseInt(gamePiece[i].style.top) - 100 == cordY && parseInt(gamePiece[i].style.left) == cordX) {
+			if (parseInt(tiles[i].style.top) - 100 == cordY && parseInt(tiles[i].style.left) == cordX) {
 				return i;
 			}
 		}
@@ -245,15 +177,14 @@ function down (x, y){ //calculates how far down a puzzlepiece should position
 		return -1;
 	} 
 }
+//determines how far down the tile position should
 
-
-
-function swap (position){ //moves the puzzle piece by switching position with an empty space
-
-	var temp = gamePiece[position].style.top;
-	gamePiece[position].style.top = spaceY;
-	spaceY = temp;
-	temp = gamePiece[position].style.left;
-	gamePiece[position].style.left = spaceX;
-	spaceX = temp;
+function swap (position){
+	var temp = tiles[position].style.top;
+	tiles[position].style.top = posY;
+	posY = temp;
+	temp = tiles[position].style.left;
+	tiles[position].style.left = posX;
+	posX = temp;
 }
+ //switches the tile at the passed position with the empty space
